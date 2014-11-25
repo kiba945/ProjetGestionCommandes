7e42ff9c1e273eacf0b13a1b42218bf112a0eaf7
Patrice <patrice945@gmail.com>
2014-11-25 15:46:31 +0100

    2ème commit

--- src/com/afpa59/patrice/appli/MesTests.java
+++ src/com/afpa59/patrice/appli/MesTests.java
@@ -8,7 +8,7 @@ public class MesTests {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	
+		System.out.println("coucou");
 		ServiceArticle serviceA;
 		IuArticle IUArt;
 		
